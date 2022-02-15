ebca4b99e72b606a3fb852dca413573392f62fa3
Peter Gallacher <peter.gallacher@hotmail.co.uk>
2022-02-15 21:08:12 +0000

    Fixed playBlackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,8 +1,9 @@
 """Module for running a Blackjack game."""
 import random
 
+
 class PlayingCardDeck:
-    """Generate a pack of cards."""    
+    """Generate a pack of cards."""
 
     def __init__(self):
         """Generate a card deck."""
@@ -12,14 +13,15 @@ class PlayingCardDeck:
         for suit in suits:
             for rank in ranks:
                 self.deck.append(rank + ' of ' + suit)
-        
+
     def shuffle(self):
         """Shuffle deck"""
-        self.deck = random.sample(self.deck, len(self.deck))            
+        self.deck = random.sample(self.deck, len(self.deck))
+
 
 class BlackJackGame:
     """Deal 2 cards to the player and the dealer, only show one of the dealer's cards."""
-    
+
     def __init__(self):
         playingCardDeck = PlayingCardDeck()
         playingCardDeck.shuffle()
@@ -28,14 +30,14 @@ class BlackJackGame:
         self._player = Player()
         self.player_finished = False
         self.dealer_finished = False
-    
-    def card_deal(self):       
+
+    def card_deal(self):
         """Deal 2 cards to the player and the dealer"""
         self._player.take_card(self._deck.pop(0))  # Player card 1
         self._dealer.take_card(self._deck.pop(0))  # Dealer card 1
         self._player.take_card(self._deck.pop(0))  # Player card 2
         self._dealer.take_card(self._deck.pop(0))  # Dealer card 2         
-        
+
     def play(self):
         "Function that displays the player's hand and one of the dealer's cards. Then gives the player a choice of stand(stop) or hit(play)"
         "After every card dealt it checks if either person is bust and continues if not. Finally, the result is displayed"
@@ -52,7 +54,7 @@ class BlackJackGame:
             self._dealer._blackjack = True
             self.player_finished = True
             self.dealer_finished = True
-            
+
         while not self.player_finished:
             if self._player.get_value() == 21:
                 self.player_finished = True
@@ -72,7 +74,7 @@ class BlackJackGame:
             self._dealer.take_card(self._deck.pop(0))
             if self._dealer.get_value() > 21:  # Check if dealer is bust
                 print("Dealer Bust!")
-                self._dealer._bust = True    
+                self._dealer._bust = True
         BlackJackGame.result(self)  # Display result of the game
 
     def result(self):
@@ -83,15 +85,18 @@ class BlackJackGame:
             print("Blackjack! You win!")
         elif self._dealer._blackjack == True:
             print("Dealer has blackjack! Dealer wins!")
-        elif (self._player.get_value() < self._dealer.get_value() and self._dealer._bust == False) or self._player._bust == True:
+        elif (
+                self._player.get_value() < self._dealer.get_value() and self._dealer._bust == False) or self._player._bust == True:
             # Dealer wins if: player is bust or has a lower card value
             print("Dealer Wins!")
-        elif (self._player.get_value() > self._dealer.get_value() and self._player._bust == False) or self._dealer._bust == True:
+        elif (
+                self._player.get_value() > self._dealer.get_value() and self._player._bust == False) or self._dealer._bust == True:
             # Player wins if: dealer is bust or has a lower card value 
             print("You Win!")
         else:
             # Otherwise it's a draw
-            print("It's a push!")  
+            print("It's a push!")
+
 
 class Hand:
 
@@ -99,11 +104,11 @@ class Hand:
         self._cards = cards
         self.card_value = 0
         self.ace_count = 0
-        self.values =  {'02': 2, '03': 3, '04': 4,
-                        '05': 5, '06': 6, '07': 7, 
-                        '08': 8, '09': 9, '10': 10, 
-                        'Ja': 10, 'Qu': 10, 'Ki': 10,
-                        'Ac': 11}
+        self.values = {'02': 2, '03': 3, '04': 4,
+                       '05': 5, '06': 6, '07': 7,
+                       '08': 8, '09': 9, '10': 10,
+                       'Ja': 10, 'Qu': 10, 'Ki': 10,
+                       'Ac': 11}
 
     def value_of_cards(self):
         """Calculates the value of the cards in play"""
@@ -116,26 +121,30 @@ class Hand:
             self.card_value -= 10
             self.ace_count -= 1
         return self.card_value
-    
+
+
 class Person:
 
     def __init__(self):
-        self._cards = [] 
+        self._cards = []
         self._hand = Hand(self._cards)
         self._bust = False
         self._blackjack = False
-                   
+
     def take_card(self, card):
         """This function allows a player to hit after the initial deal"""
         self._cards.append(card)
 
     def get_value(self):
         """Gets the value of each players' hand"""
-        hand = Hand(self._cards) 
+        hand = Hand(self._cards)
         return hand.value_of_cards()
+
+
 class Player(Person):
     def __init__(self):
-        super().__init__()    
+        super().__init__()
+
 
 class Dealer(Person):
     def __init__(self):
